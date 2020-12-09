5c2206232124baf7530c52ec23efd4e3d4edff99
WojtAcht <wachtelik@gmail.com>
2020-12-09 10:53:48 +0100

    Start lab4

---
+++ lab2/SymbolTable.py
@@ -0,0 +1,31 @@
+class VariableSymbol():
+
+    def __init__(self, name, ttype):
+        self.name = name
+        self.type = ttype
+
+
+class SymbolTable(object):
+
+    def __init__(self, parent, name): # parent scope and symbol table name
+        self.parent = parent
+        self.name = name
+        self.symbols = {}
+
+    def put(self, name, symbol): # put variable symbol or fundef under <name> entry
+        self.symbols[name] = symbol
+
+    def get(self, name): # get variable symbol or fundef from <name> entry
+        if name in self.symbols: return self.symbols[name]
+        if self.parent: return self.parent.get(name)
+        return None
+
+    def getParentScope(self):
+        return self.parent
+    
+    #def pushScope(self, name):
+        
+    
+    #def popScope(self):
+
+    
---
+++ lab2/TypeChecker.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+
+# ---------------- TYPES -----------------
+
+INT = 'INT'
+FLOAT = 'FLOAT'
+STRING = 'STRING'
+RANGE = 'RANGE'
+ARRAY = 'ARRAY'
+BOOL = 'BOOL'
+
+# ----------- OPERATION TYPES: -----------
+
+ttype = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: None)))
+
+# Arithmetic operators:
+for op in ['+', '-', '*', '/']:
+    ttype[op][INT][INT] = INT
+    ttype[op][FLOAT][INT] = ttype[op][INT][FLOAT] = ttype[op][FLOAT][FLOAT] = FLOAT
+
+ttype['+'][STRING][STRING] = STRING
+
+# Boolean operators:
+for op in ['<', '>', '==', '!=', '<=', '>=']:
+    ttype[op][INT][INT] = BOOL
+    ttype[op][FLOAT][INT] = ttype[op][INT][FLOAT] = ttype[op][FLOAT][FLOAT] = BOOL
+
+# We can also compare strings and arrays:
+for op in ['==', '!=']:
+    ttype[op][STRING][STRING] = BOOL
+    ttype[op][ARRAY][ARRAY] = BOOL
+
+class NodeVisitor(object):
+
+    def visit(self, node):
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node)
+
+
+    def generic_visit(self, node):        # Called if no explicit visitor function exists for a node.
+        if isinstance(node, list):
+            for elem in node:
+                self.visit(elem)
+        else:
+            for child in node.children:
+                if isinstance(child, list):
+                    for item in child:
+                        if isinstance(item, AST.Node):
+                            self.visit(item)
+                elif isinstance(child, AST.Node):
+                    self.visit(child)
+
+    # simpler version of generic_visit, not so general
+    #def generic_visit(self, node):
+    #    for child in node.children:
+    #        self.visit(child)
+
+
+
+class TypeChecker(NodeVisitor):
+
+    def __init__(self):
+        self.table = SymbolTable(None, "table")
+
+    def visit_Int(self, node):
+        return INT
+
+    def visit_Float(self, node):
+        return FLOAT
+    
+    def visit_String(self, node):
+        return STRING
+ 
+    def visit_ID(self, node):
+        symbol = self.table.get(node.id)
+        if symbol:
+            return symbol.type
+        else:
+            print(f"{node.id} - undefined!")
+            return None
+            
+    def visit_Binary(self, node):
+        pass
+
+    def visit_Assign(self, node):
+        pass
+
+    def visit_If(self, node):
+        pass
+
+    def visit_While(self, node):
+        pass
+
+    def visit_Range(self, node):
+        pass
+
+    def visit_For(self, node):
+        pass
